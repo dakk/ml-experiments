fb5b2007d34c392fcb524254d852ebc92c7f75e5
Davide Gessa (dakk) <gessadavide@gmail.com>
2020-10-20 11:01:34 +0200

    transformer testing

--- gpt2_it_transformer.py
+++ gpt2_it_transformer.py
@@ -1,9 +1,28 @@
-from transformers import AutoTokenizer
-from transformers import AutoModelWithLMHead
-
+from transformers import AutoTokenizer, AutoModelWithLMHead, pipeline, GPT2Tokenizer
 
 tokenizer = AutoTokenizer.from_pretrained("LorenzoDeMattei/GePpeTto")
-			
 model = AutoModelWithLMHead.from_pretrained("LorenzoDeMattei/GePpeTto")
-			
-		
+
+text_generator = pipeline('text-generation', model=model, tokenizer=tokenizer)
+prompts = [
+    "Harry vide Malfoy che percorreva il corridoio con fare sospetto; non",
+    ""
+]
+
+
+samples_outputs = text_generator(
+    prompts,
+    do_sample=True,
+    max_length=80,
+    top_k=50,
+    top_p=0.95,
+    num_return_sequences=3
+)
+
+
+for i, sample_outputs in enumerate(samples_outputs):
+    print(100 * '-')
+    print("Prompt:", prompts[i])
+    for sample_output in sample_outputs:
+        print("Sample:", sample_output['generated_text'])
+        print()
